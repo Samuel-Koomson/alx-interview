8a670f8267bc1cde75ead6ec16fc4089bf7d3a8e
Samuel Koomson <kskoomson1@gmail.com>
2023-09-28 16:05:56 +0000

    More requirement changes made

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,10 +7,6 @@
 
     Returns:
     list: A list of lists which represents Pascal's triangle.
-
-    Example:
-    pascals_triangle(5)
-    [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
     """
 def pascal_triangle(n):
     triangle = []
